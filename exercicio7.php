7cdf1c88ca401c85bc63cd2606b1be7533457fcc
eloisaper <eloisaperuzzo2000@outlook.com>
2023-04-25 10:13:35 -0300

    alteração 4

--- exercicio7.php
+++ exercicio7.php
@@ -1,7 +1,8 @@
 <?php
     $idade = $_GET['Idade'];
+    $nome = $_GET['Nome'];
     $dias = $idade * 365;
     
-    echo "Sua idade em dias: $dias";
+    echo "Eu sou $nome e tenho $dias dias na terra!";
 
 ?>
